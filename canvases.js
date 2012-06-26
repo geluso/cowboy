7789272aeca8f9200f09df8d6805bb0436f949d2
Steve Geluso <stevegeluso@gmail.com>
2012-06-25 18:28:26 -0700

    labels, better actor drawing

---
+++ canvases.js
@@ -0,0 +1,8 @@
+function init(layers) {
+  for (var i = 0; i < layers.length; i++) {
+    var layer = document.getElementById(layers[i]);
+    background.width = WIDTH;
+    background.height = HEIGHT;
+    var ctx = background.getContext("2d");
+  }
+}

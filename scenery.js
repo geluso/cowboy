4f49ca9d21a8145a955a42d77d790bf18e0c3a9f
Steve Geluso <stevegeluso@gmail.com>
2015-06-27 13:25:52 -0700

    natives wander around camp

--- scenery.js
+++ scenery.js
@@ -152,6 +152,7 @@ function birth_natives(ctx, a) {
 
 function birth_native_dude(ctx, a, x, y) {
   var nativedude = {
+    alive: true,
     image: function () {
       return IMAGES["native"]; 
     },
@@ -160,8 +161,54 @@ function birth_native_dude(ctx, a, x, y) {
     label: function() { return "native"; },
     draw: function (ctx) {
       draw_actor(ctx, this);
-    }
+    },
+    way_x: undefined,
+    way_y: undefined,
+    actions: [],
+    step: function() {
+      var step = .6;
+      return step;
+    },
+    stop: function () {
+      if (nativedude.actions) {
+        clear_intervals(nativedude.actions);
+      }
+      nativedude.actions = [];
+      nativedude.way_x = undefined;
+      nativedude.way_y = undefined;
+
+      if (!nativedude.alive) {
+        return;
+      }
+
+      var delay = Math.random() * 10;
+      var move = Math.random() * 10;
+
+      if (move < 6) {
+        // don't always pick a destination
+        setTimeout(nativedude.stop, delay * 1000);
+      } else {
+        var x = 360;
+        var y = 310;
+        var spread = 75;
+
+        if (Math.random() > .5) {
+          x = x + Math.random() * spread;
+        } else {
+          x = x - Math.random() * spread;
+        }
+
+        if (Math.random() > .5) {
+          y = y + Math.random() * spread;
+        } else {
+          y = y - Math.random() * spread;
+        }
+
+        set_waypoint(nativedude, x, y);
+      }
+    },
   };
+  nativedude.stop();
   a.push(nativedude);
   return nativedude;
 }

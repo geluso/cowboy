391ee587d3e72607687505295f296cca552cc98f
Steve Geluso <stevegeluso@gmail.com>
2018-09-29 19:07:24 -0700

    refactoring horse to drawable

--- js/actors/horse.js
+++ js/actors/horse.js
@@ -1,91 +1,106 @@
 var HORSE;
 
 function birth_horse(ctx, a) {
-  HORSE = {
-    get image() {
-      if (this.alive) {
-        return IMAGES[["horse_north", "horse_east", "horse_south", "horse_west"][this.direction]];
-      } else {
-        return IMAGES["dead_horse"];
-      }
-    },
-    x: 20 + 50,
-    y: 20 + 50,
-    way_x: undefined,
-    way_y: undefined,
-    actions: [],
-    get label() { return "horse"; },
-    step: function() { return 3; },
-    stop: function () {
-      clear_intervals(this.actions);
-      this.actions = [];
-      this.way_x = undefined;
-      this.way_y = undefined;
+  HORSE = new Horse();
+  a.push(HORSE);
+}
 
-      // if the cowboy is still far away then set a new waypoint toward him.
-      if (Math.abs(COWBOY.x - HORSE.x) > 1 ||
-          Math.abs(COWBOY.y - HORSE.y) > 1) {
-        set_waypoint(HORSE, COWBOY.x, COWBOY.y);
-      } else {
-        HORSE.bridle();
-      }
-    },
-    draw: function (ctx) {
-      if (this.way_x !== undefined && this.way_y !== undefined) {
-          if (Math.abs(COWBOY.x - HORSE.x) < 10 &&
-              Math.abs(COWBOY.y - HORSE.y) < 10) {
-            HORSE.bridle();
-          }
-      }
+class Horse extends Drawable {
+  constructor(x, y) {
+    super(x, y, "horse");
+    this.x = 20 + 50
+    this.y = 20 + 50
+    this.way_x = undefined
+    this.way_y = undefined
+    this.actions = []
+    this.label = "horse"
 
-      if (!this.alive) {
-        ctx.drawImage(this.image,
-            this.frame_width * this.frame, 0, this.frame_width, this.frame_height,
-            this.x - Math.floor(this.frame_width / 2),
-            this.y - Math.floor(this.frame_height / 2),
-            this.frame_width, this.frame_height);
-      } else if (this.unbridled) {
-        draw_actor(ctx, this);
-      }
-    },
-    alive: true,
-    unbridled: true,
-    direction: EAST,
-    // death animation
-    frame_width: 17,
-    frame_height: 12,
-    frames: 7,
-    frame: 0,
-    delay: 2000,
-    bridle: function() {
-        COWBOY.horse = true;
-        HORSE.unbridled = false;
-        HORSE.x = COWBOY.x;
-        HORSE.y = COWBOY.y;
+    this.alive = true
+    this.unbridled = true
+    this.direction = EAST
+    this.frame_width = 17
+    this.frame_height = 12
+    this.frames = 7
+    this.frame = 0
+    this.delay = 2000
+  }
 
-        HORSE.way_x = undefined;
-        HORSE.way_y = undefined;
-    },
-    unbridle: function() {
-        COWBOY.horse = false;
-        HORSE.unbridled = true;
-        HORSE.x = COWBOY.x;
-        HORSE.y = COWBOY.y;
-    },
-    kill: function() {
-      if (this.alive) {
-        this.alive = false;
-        this.decay(this);
-      }
-    },
-    decay: function (actor) {
-      actor.frame++;
-      if (actor.frame < actor.frames) {
-        setTimeout(function() {
-          actor.decay(actor);
-        }, actor.delay);
-      }
+  get image() {
+    if (this.alive) {
+      return IMAGES[["horse_north", "horse_east", "horse_south", "horse_west"][this.direction]];
+    } else {
+      return IMAGES["dead_horse"];
     }
   }
-  a.push(HORSE);
+
+  step() {
+    return 3;
+  }
+
+  stop() {
+    clear_intervals(this.actions);
+    this.actions = [];
+    this.way_x = undefined;
+    this.way_y = undefined;
+
+    // if the cowboy is still far away then set a new waypoint toward him.
+    if (Math.abs(COWBOY.x - HORSE.x) > 1 ||
+        Math.abs(COWBOY.y - HORSE.y) > 1) {
+      set_waypoint(HORSE, COWBOY.x, COWBOY.y);
+    } else {
+      HORSE.bridle();
+    }
+  }
+
+  draw(ctx) {
+    if (this.way_x !== undefined && this.way_y !== undefined) {
+        if (Math.abs(COWBOY.x - HORSE.x) < 10 &&
+            Math.abs(COWBOY.y - HORSE.y) < 10) {
+          HORSE.bridle();
+        }
+    }
+
+    if (!this.alive) {
+      ctx.drawImage(this.image,
+          this.frame_width * this.frame, 0, this.frame_width, this.frame_height,
+          this.x - Math.floor(this.frame_width / 2),
+          this.y - Math.floor(this.frame_height / 2),
+          this.frame_width, this.frame_height);
+    } else if (this.unbridled) {
+      draw_actor(ctx, this);
+    }
+  }
+
+  bridle() {
+      COWBOY.horse = true;
+      HORSE.unbridled = false;
+      HORSE.x = COWBOY.x;
+      HORSE.y = COWBOY.y;
+
+      HORSE.way_x = undefined;
+      HORSE.way_y = undefined;
+  }
+
+  unbridle() {
+      COWBOY.horse = false;
+      HORSE.unbridled = true;
+      HORSE.x = COWBOY.x;
+      HORSE.y = COWBOY.y;
+  }
+
+  kill() {
+    if (this.alive) {
+      this.alive = false;
+      this.decay(this);
+    }
+  }
+
+  decay(actor) {
+    actor.frame++;
+    if (actor.frame < actor.frames) {
+      setTimeout(function() {
+        actor.decay(actor);
+      }, actor.delay);
+    }
+  }
 }
